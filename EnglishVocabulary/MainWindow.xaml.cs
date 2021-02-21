2ff29031a1b7cb1d4260b7e8923d639304b6c412
Maksim <borisovets.maksim@gmail.com>
2021-02-21 22:04:20 +0300

    point where I -have ScrollViewer style -have no ComboBox style -have animation for company logo image (color change)

--- EnglishVocabulary/MainWindow.xaml.cs
+++ EnglishVocabulary/MainWindow.xaml.cs
@@ -13,37 +13,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace EnglishVocabulary
 {
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
 
-            ActivateComboBoxes();
+            //ActivateComboBoxes();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            grdMenuItemAbout.MouseDown += (s, a) =>
-            {
-                grdMenuItemAbout.Background = rsrcClrHeaderBackground.Fill;
-            };
-            grdMenuItemAbout.MouseUp += (s, a) =>
-            {
-                grdMenuItemAbout.Background = rsrcClrMenuButtonEnter.Fill;
-            };
-            grdMenuItemAbout.MouseEnter += (s, a) =>
-            {
-                grdMenuItemAbout.Background = rsrcClrMenuButtonEnter.Fill;
-            };
-            grdMenuItemAbout.MouseLeave += (s, a) =>
-            {
-                grdMenuItemAbout.Background = rsrcClrMenuButton.Fill;
-            };
+            GhangeImageColor();
 
+            
 
 
 
@@ -59,6 +47,51 @@ namespace EnglishVocabulary
             //{
             //    MessageBox.Show(item);
             //}
+        }
+
+        private void GhangeImageColor()
+        {
+            DispatcherTimer t = new DispatcherTimer();
+            t.Interval = new TimeSpan(0, 0, 0, 0, 50);
+
+            byte maxValue = 255;
+            byte minValue = 75;
+            byte speed = 5;
+            byte r = maxValue;
+            byte g = minValue;
+            byte b = minValue;
+
+
+            t.Tick += (s, a) =>
+            {
+                if (r == maxValue && g < maxValue && b == minValue)
+                {
+                    g += speed;
+                }
+                else if (r > minValue && g == maxValue && b == minValue)
+                {
+                    r -= speed;
+                }
+                else if (r == minValue && g == maxValue && b < maxValue)
+                {
+                    b += speed;
+                }
+                else if (r == minValue && g > minValue && b == maxValue)
+                {
+                    g -= speed;
+                }
+                else if (r < maxValue && g == minValue && b == maxValue)
+                {
+                    r += speed;
+                }
+                else if (r == maxValue && g == minValue && b > minValue)
+                {
+                    b -= speed;
+                }
+
+                rctMenuImage.Fill = new SolidColorBrush(Color.FromRgb(r, g, b));
+            };
+            t.Start();
         }
     }
 }
