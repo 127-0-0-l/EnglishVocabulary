d98da99fa1f6745220348581b33aa2ddbb908646
Maksim <borisovets.maksim@gmail.com>
2021-02-22 22:50:38 +0300

    add loading about information

--- EnglishVocabulary/About.cs
+++ EnglishVocabulary/About.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace EnglishVocabulary
 {
     public partial class MainWindow
     {
+        /// <summary>
+        /// Load text from "About.txt" to about section.
+        /// </summary>
         private void LoadAbout()
         {
+            string path = Environment.CurrentDirectory + @"\..\..\Assets\About.txt";
 
+            using (StreamReader file = new StreamReader(path))
+            {
+                rtbHelpText.AppendText(file.ReadToEnd());
+            }
         }
     }
 }
