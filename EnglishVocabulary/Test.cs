7a59286cd7f7769d8494567c4f60fd05813c778f
Maksim <borisovets.maksim@gmail.com>
2021-02-25 14:02:17 +0300

    change grids (buttons) to real buttons in Test section

--- EnglishVocabulary/Test.cs
+++ EnglishVocabulary/Test.cs
@@ -10,41 +10,6 @@ namespace EnglishVocabulary
 {
     public partial class MainWindow
     {
-        private void grdTestHeaderBtnStart_MouseEnter(object sender, MouseEventArgs e)
-        {
-            grdTestHeaderBtnStart.Background = colorButtonEnter.Fill;
-        }
-
-        private void grdTestHeaderBtnStart_MouseLeave(object sender, MouseEventArgs e)
-        {
-            grdTestHeaderBtnStart.Background = colorButton.Fill;
-        }
-
-        private void grdTestHeaderBtnStart_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            grdTestHeaderBtnStart.Background = colorButtonClick.Fill;
-        }
-
-        private void grdTestHeaderBtnStart_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            grdTestHeaderBtnStart.Background = colorButtonEnter.Fill;
-            grdTestHeaderBtnStart.Background = colorButtonDisabled.Fill;
-            tbTestHeaderBtnStartText.Foreground = colorButtonTextDisabled.Fill;
-            grdTestHeaderBtnStart.IsEnabled = false;
-        }
-
-        private void grdTestHeaderBtnStart_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (grdTestHeaderBtnStart.IsEnabled == false)
-            {
-                grdTestHeaderBtnStart.Background = colorButtonDisabled.Fill;
-                tbTestHeaderBtnStartText.Foreground = colorButtonTextDisabled.Fill;
-            }
-            else
-            {
-                grdTestHeaderBtnStart.Background = colorButton.Fill;
-                tbTestHeaderBtnStartText.Foreground = colorButtonText.Fill;
-            }
-        }
+        
     }
 }
