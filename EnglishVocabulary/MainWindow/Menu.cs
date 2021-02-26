25456d9dcd98ca70a3814c44e5fd80225b11cf95
Maksim <borisovets.maksim@gmail.com>
2021-02-26 11:21:07 +0300

    delete button style. there some bugs, but I can't recognize it

--- EnglishVocabulary/MainWindow/Menu.cs
+++ EnglishVocabulary/MainWindow/Menu.cs
@@ -15,6 +15,7 @@ namespace EnglishVocabulary
         private void btnMenuTest_Click(object sender, RoutedEventArgs e)
         {
             SetActiveSection(grdTest);
+            btnMenuTest.IsEnabled = false;
         }
 
         /// <summary>
