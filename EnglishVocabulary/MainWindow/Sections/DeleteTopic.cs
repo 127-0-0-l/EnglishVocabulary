bcb80ccbe1f063a0455023b566cac1591a911968
Maksim <borisovets.maksim@gmail.com>
2021-03-15 17:03:07 +0300

    made subtopic deletion

--- EnglishVocabulary/MainWindow/Sections/DeleteTopic.cs
+++ EnglishVocabulary/MainWindow/Sections/DeleteTopic.cs
@@ -1,13 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace EnglishVocabulary
 {
     public partial class MainWindow
     {
+        private void cbDeleteTopicTopic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbDeleteTopicTopic.SelectedItem != null)
+            {
+                string topicName = cbDeleteTopicTopic.SelectedItem.ToString();
+                cbDeleteTopicSubtopic.ItemsSource = DataBase.GetSubtopics(topicName);
+                cbDeleteTopicSubtopic.IsEnabled = true;
+            }
+        }
 
+        private void cbDeleteTopicSubtopic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbDeleteTopicSubtopic.SelectedItem != null)
+            {
+                btnDeleteTopicDelete.IsEnabled = true;
+            }
+        }
+
+        private void btnDeleteTopicDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string topicName = cbDeleteTopicTopic.SelectedItem.ToString();
+            string subtopicName = cbDeleteTopicSubtopic.SelectedItem.ToString();
+            DataBase.DeleteSubtopic(topicName, subtopicName);
+
+            ResetDeleteTopicSection();
+        }
+
+        private void ResetDeleteTopicSection()
+        {
+            cbDeleteTopicTopic.Text = "";
+            cbDeleteTopicSubtopic.Text = "";
+            cbDeleteTopicSubtopic.IsEnabled = false;
+            btnDeleteTopicDelete.IsEnabled = false;
+        }
     }
 }
